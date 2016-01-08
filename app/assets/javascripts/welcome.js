de98f76779193a97fbd37df3d0c6da47d0505f94
sydneyhodge <sydneyahodge@gmail.com>
2016-01-08 14:26:45 -0500

    Updated coming soon page.

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#normal-delay").fadeIn(2500);
+	$("#long-delay").fadeIn(5000);
+	$("#xlong-delay").fadeIn(5500);
+	$("#xxlong-delay").fadeIn(6000);
+	$("#xxxlong-delay").fadeIn(6500);
+});
