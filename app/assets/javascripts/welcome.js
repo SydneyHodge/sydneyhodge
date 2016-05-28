c5767f2f3c0a99a671e92d79f97b73c6be6f9275
sydneyhodge <sydneyahodge@gmail.com>
2016-05-27 23:46:01 -0400

    Social and contact pages complete. JS and design for the navigation menu.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,23 +1,44 @@
-$(document).ready(function() {
-	$("#normal-delay").fadeIn(2500, function() {
-		$("#long-delay").fadeIn(2500, function() {
-			$("#xlong-delay").fadeIn(1500, function() {
-				$("#xxlong-delay").fadeIn(1500, function() {
-					$("#xxxlong-delay").fadeIn(1500);					
-				});
-			});
-		});
+var ready = function() {
+	$(".menu-item").click(function() {
+		// Clears elements within these divs.
+		$("#additional-info").children().not(".info-hr").remove();
+		$("#right-content .content").children().remove();
+
+		// Retrieves the object that was clicked.
+		$object = $(this).attr("id");
+
+		// Adds the appropriate content to the respective div elements.
+		$("#additional-info").append($("#left-responses ." + $object + "-info").clone());
+		$("#right-content .content").append($("#right-responses ." + $object + "-info").clone());
+
+		// // Removes the hover effect from all elements, except for the one selected.
+		// $(".menu-item").not($(this)).each(function() {
+		// 	$(this).removeClass("menu-item-hover");
+		// });
+
+		// // Adds the hover effect to @code($this).
+		// $(this).addClass("menu-item-hover")
 	});
 
-   $('.delay').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
+	// Removes any elements in the @code(#additional-info) div.
+	$(".name").click(function() {
+		$("#additional-info").children().not(".info-hr").remove();
+	});
 
-   $('.delay').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       });
-   });
-});
+	$(".logo").mouseleave(function() {
+		// $object = $(this).attr("id");
+		// $($object + "-color img").replaceWith($($object + " img"));
+	});
+};
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
+
+function change_image_color(value) {
+	$("#" + value).before($("#" + value + "-color").clone()).hide()
+}
+
+function change_image(value) {
+	$("#" + value + "-color").remove();
+	$("#" + value).show();
+}
